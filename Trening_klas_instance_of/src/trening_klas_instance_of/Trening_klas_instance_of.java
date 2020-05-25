ab81815d7f1b3ac543e7a60caccf8756af370ec0
Damian Smolinski <damian.smolinski01@gmail.com>
2020-05-25 19:49:59 +0200

    wykorzystałem tu wlasnie instance of i wtedy ten ulepszony for dziala

--- Trening_klas_instance_of/src/trening_klas_instance_of/Trening_klas_instance_of.java
+++ Trening_klas_instance_of/src/trening_klas_instance_of/Trening_klas_instance_of.java
@@ -12,7 +12,7 @@ abstract class Osoba
         this.nazwisko=nazwisko;
     }
     
-    
+    abstract void getInfo();
    
 }
 class Student extends Osoba
@@ -23,8 +23,17 @@ class Student extends Osoba
        
     }
 
-  
-  
+    @Override
+    void getInfo() 
+    {
+        System.out.println("Jestem studentem");
+        System.out.println("Mam na imie: "+imie);
+        System.out.println("Mam na nazwisko: "+nazwisko);
+    }
+    void learn()
+    {
+        System.out.println("Ucze się przecież");
+    }
 }
 class Pracownik extends Osoba
 {
@@ -36,7 +45,18 @@ class Pracownik extends Osoba
     }
     double wynagrodzenie;
 
-   
+    @Override
+    void getInfo() 
+    {
+        System.out.println("Jestem pracownikiem");
+        System.out.println("Mam na imie: "+imie);
+        System.out.println("Mam na nazwisko: "+nazwisko);
+        System.out.println("zarabiam: "+wynagrodzenie);
+    }
+    void pracuj()
+    {
+        System.out.println("Pracuje se");
+    }
 }
 
 public class Trening_klas_instance_of 
@@ -48,3 +68,28 @@ public class Trening_klas_instance_of
         osoby[0]=new Student("Filip","Stelmański");
         osoby[1]=new Pracownik("Andrzej","Jakiśtam",1000.2);
         for(Osoba person:osoby)
+        {
+            if(person instanceof Pracownik)
+            {
+             Pracownik tmp = ((Pracownik)person);
+             tmp.getInfo();
+             tmp.pracuj();
+//            ((Pracownik)person).getInfo();
+//            ((Pracownik)person).pracuj();
+            
+            }
+            else if(person instanceof Student)
+            {
+                Student tmp = ((Student)person);
+                tmp.learn();
+                tmp.getInfo();
+//                ((Student)person).getInfo();
+//                ((Student)person).learn();
+//                
+            }
+                
+        }
+        
+    }
+    
+}
