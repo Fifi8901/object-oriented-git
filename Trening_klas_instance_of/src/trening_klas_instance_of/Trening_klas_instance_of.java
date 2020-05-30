5782595b69927f9f6f63d588615dd2b16f0774a6
Fifi8901 <filiphemki@gmail.com>
2020-05-30 21:13:04 +0200

    uzupelniony trening klas

--- Trening_klas_instance_of/src/trening_klas_instance_of/Trening_klas_instance_of.java
+++ Trening_klas_instance_of/src/trening_klas_instance_of/Trening_klas_instance_of.java
@@ -23,8 +23,22 @@ class Student extends Osoba
        
     }
 
+<<<<<<< HEAD
   
   
+=======
+    @Override
+    void getInfo() 
+    {
+        System.out.println("Jestem studentem");
+        System.out.println("Mam na imie: "+imie);
+        System.out.println("Mam na nazwisko: "+nazwisko);
+    }
+    void learn()
+    {
+        System.out.println("Ucze się przecież");
+    }
+>>>>>>> ab81815d7f1b3ac543e7a60caccf8756af370ec0
 }
 class Pracownik extends Osoba
 {
@@ -36,7 +50,22 @@ class Pracownik extends Osoba
     }
     double wynagrodzenie;
 
+<<<<<<< HEAD
    
+=======
+    @Override
+    void getInfo() 
+    {
+        System.out.println("Jestem pracownikiem");
+        System.out.println("Mam na imie: "+imie);
+        System.out.println("Mam na nazwisko: "+nazwisko);
+        System.out.println("zarabiam: "+wynagrodzenie);
+    }
+    void pracuj()
+    {
+        System.out.println("Pracuje se");
+    }
+>>>>>>> ab81815d7f1b3ac543e7a60caccf8756af370ec0
 }
 
 public class Trening_klas_instance_of 
@@ -47,4 +76,33 @@ public class Trening_klas_instance_of
         Osoba[]osoby=new Osoba[5];
         osoby[0]=new Student("Filip","Stelmański");
         osoby[1]=new Pracownik("Andrzej","Jakiśtam",1000.2);
+<<<<<<< HEAD
         for(Osoba person:osoby)
+=======
+        for(Osoba person:osoby)
+        {
+            if(person instanceof Pracownik)
+            {
+             Pracownik tmp = ((Pracownik)person);
+             tmp.getInfo();
+             tmp.pracuj();
+//            ((Pracownik)person).getInfo();
+//            ((Pracownik)person).pracuj();
+            
+            }
+            else if(person instanceof Student)
+            {
+                Student tmp = ((Student)person);
+                tmp.learn();
+                tmp.getInfo();
+//                ((Student)person).getInfo();
+//                ((Student)person).learn();
+//                
+            }
+                
+        }
+        
+    }
+    
+}
+>>>>>>> ab81815d7f1b3ac543e7a60caccf8756af370ec0
