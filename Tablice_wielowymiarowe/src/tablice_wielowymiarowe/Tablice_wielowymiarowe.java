64436a9afd783e88c502d11e1ea0b0390e50122a
michal <michalskowronski99029@gmail.com>
2020-05-31 01:25:46 +0200

    Dodaję tablice wielo wymiarowe

---
+++ Tablice_wielowymiarowe/src/tablice_wielowymiarowe/Tablice_wielowymiarowe.java
@@ -0,0 +1,27 @@
+package tablice_wielowymiarowe;
+
+public class Tablice_wielowymiarowe 
+{
+
+    public static void main(String[] args) 
+    {
+        int[][] tab1 = new int[5][4];
+        tab1[2][3] = 15;
+        
+        System.out.println(tab1[2][3]);
+        
+        //odczytujemy najpierw wierszami, potem kolumnami
+        int[][] tab2 = 
+        {
+                {4,5,6,7,8,12},
+                {62,12,13,43,12,7},
+                {54,43,56,78,90,91},
+                {123, 124, 56, 89, 10, 20},
+                {98, 87, 65, 47, 82, 19},
+                {10, 20, 30, 40, 50, 60},
+                                    
+       };
+        System.out.println(tab2[3][3]);
+    }
+    
+}
