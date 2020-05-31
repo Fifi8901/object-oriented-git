7c655b8b977ddd88a433d262f05d49235807f441
Damian Smolinski <damian.smolinski01@gmail.com>
2020-05-31 07:07:58 +0200

    Dodałem na nowo i Skonfigurowałem Butelke bo się zaczytała w złym formacie

---
+++ Butelka_new/src/butelka_new/Butelka_new.java
@@ -0,0 +1,111 @@
+package butelka;
+
+public class Butelka 
+{
+
+double ileLitrow;
+double pojemnoscButelki;
+static int ileButelek=50;
+double gdziePrzelac;
+
+
+
+
+
+Butelka()
+{
+    ileLitrow=10;
+    pojemnoscButelki=20;
+}
+    
+ Butelka(double ileLitrow, double pojemnoscButelki)
+     {
+        this.ileLitrow+=ileLitrow;
+        this.pojemnoscButelki+=pojemnoscButelki;
+     }
+  void wlej(double ilosc)
+ {
+     if(ileLitrow+ilosc>pojemnoscButelki)
+     {
+         System.out.println("Nalałem na maxa. Zostało:  "+(ilosc-(pojemnoscButelki-ileLitrow)));
+         ileLitrow+=(pojemnoscButelki-ileLitrow);
+      }
+     
+     else
+     {
+     this.ileLitrow+=ilosc;
+     }
+     
+
+ }
+  void wylej(double ilosc)
+ {
+     if(ilosc>ileLitrow)
+     {
+         this.ileLitrow-=ileLitrow;
+         System.out.println("Więcej z niej się nie da wylać");
+         
+     }
+     else
+     {
+     this.ileLitrow-=ilosc;
+     }
+     
+     
+ }
+ 
+   void przelej (double ilosc, Butelka gdziePrzelac)
+   {
+
+       
+       this.wylej(ilosc);
+       gdziePrzelac.wlej(ilosc);
+       
+       
+      
+   }
+
+    public static void main(String[] args) 
+    {
+        
+       Butelka[]butelka = new Butelka[ileButelek];
+       for(int i=0; i<ileButelek; i++)
+       {
+           if(i%2!=0)
+           {
+               butelka[i] = new Butelka (12, 40);
+           }
+           else if (i%3!=0)
+           {
+               butelka[i]= new Butelka (8, 12.4);
+           }
+           else
+               
+            butelka[i] = new Butelka();
+          
+       }
+        System.out.println(butelka[1].ileLitrow+ " "+ butelka[1].pojemnoscButelki);
+        System.out.println(butelka[2].ileLitrow+ " "+ butelka[2].pojemnoscButelki);
+        butelka[1].przelej(11, butelka[2]);
+        System.out.println(butelka[1].ileLitrow+ " "+ butelka[1].pojemnoscButelki);
+        System.out.println(butelka[2].ileLitrow+ " "+ butelka[2].pojemnoscButelki);
+        butelka[1].wylej(50);
+        System.out.println(butelka[1].ileLitrow+ " "+ butelka[1].pojemnoscButelki);
+        butelka[1].wlej(45);
+        System.out.println(butelka[1].ileLitrow+ " "+ butelka[1].pojemnoscButelki);
+       
+      
+      
+    }
+
+    public Butelka(double ileLitrow, double pojemnoscButelki, double gdziePrzelac) {
+        this.ileLitrow = ileLitrow;
+        this.pojemnoscButelki = pojemnoscButelki;
+        this.gdziePrzelac = gdziePrzelac;
+    }
+  
+    
+
+    
+}
+
