7c0c2d0e7e6c14dff51e8399fbb33a06aaad9bab
Damian Smolinski <damian.smolinski01@gmail.com>
2020-05-31 07:24:18 +0200

    zmieniłem kod w butelka_new, dodałem toString() - teraz działa

--- Butelka_new/src/butelka_new/Butelka_new.java
+++ Butelka_new/src/butelka_new/Butelka_new.java
@@ -1,6 +1,6 @@
-package butelka;
+package butelka_new;
 
-public class Butelka 
+public class Butelka_new
 {
 
 double ileLitrow;
@@ -12,13 +12,13 @@ double gdziePrzelac;
 
 
 
-Butelka()
+Butelka_new()
 {
     ileLitrow=10;
     pojemnoscButelki=20;
 }
     
- Butelka(double ileLitrow, double pojemnoscButelki)
+ Butelka_new(double ileLitrow, double pojemnoscButelki)
      {
         this.ileLitrow+=ileLitrow;
         this.pojemnoscButelki+=pojemnoscButelki;
@@ -54,7 +54,7 @@ Butelka()
      
  }
  
-   void przelej (double ilosc, Butelka gdziePrzelac)
+   void przelej (double ilosc, Butelka_new gdziePrzelac)
    {
 
        
@@ -68,20 +68,20 @@ Butelka()
     public static void main(String[] args) 
     {
         
-       Butelka[]butelka = new Butelka[ileButelek];
+       Butelka_new[]butelka = new Butelka_new[ileButelek];
        for(int i=0; i<ileButelek; i++)
        {
            if(i%2!=0)
            {
-               butelka[i] = new Butelka (12, 40);
+               butelka[i] = new Butelka_new (12, 40);
            }
            else if (i%3!=0)
            {
-               butelka[i]= new Butelka (8, 12.4);
+               butelka[i]= new Butelka_new (8, 12.4);
            }
            else
                
-            butelka[i] = new Butelka();
+            butelka[i] = new Butelka_new();
           
        }
         System.out.println(butelka[1].ileLitrow+ " "+ butelka[1].pojemnoscButelki);
@@ -98,11 +98,52 @@ Butelka()
       
     }
 
-    public Butelka(double ileLitrow, double pojemnoscButelki, double gdziePrzelac) {
+    public Butelka_new(double ileLitrow, double pojemnoscButelki, double gdziePrzelac) {
         this.ileLitrow = ileLitrow;
         this.pojemnoscButelki = pojemnoscButelki;
         this.gdziePrzelac = gdziePrzelac;
     }
+
+    public double getIleLitrow() {
+        return ileLitrow;
+    }
+
+    public void setIleLitrow(double ileLitrow) {
+        this.ileLitrow = ileLitrow;
+    }
+
+    public double getPojemnoscButelki() {
+        return pojemnoscButelki;
+    }
+
+    public void setPojemnoscButelki(double pojemnoscButelki) {
+        this.pojemnoscButelki = pojemnoscButelki;
+    }
+
+    public static int getIleButelek() {
+        return ileButelek;
+    }
+
+    public static void setIleButelek(int ileButelek) {
+        Butelka_new.ileButelek = ileButelek;
+    }
+
+    public double getGdziePrzelac() {
+        return gdziePrzelac;
+    }
+
+    public void setGdziePrzelac(double gdziePrzelac) {
+        this.gdziePrzelac = gdziePrzelac;
+    }
+
+    @Override
+    public String toString() {
+        return "Butelka{" + "ileLitrow=" + ileLitrow + ", pojemnoscButelki=" + pojemnoscButelki + ", gdziePrzelac=" + gdziePrzelac + '}';
+    }
+    
+    
+    
+    
   
     
 
